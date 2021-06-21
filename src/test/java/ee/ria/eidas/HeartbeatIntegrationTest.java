c35d14732a886d999947f36394f28e21263d55a6
naare <aare.nurm@nortal.com>
2021-06-21 14:49:26 +0300

    TARA2-210 more generic heartbeat test.

--- src/test/java/ee/ria/eidas/HeartbeatIntegrationTest.java
+++ src/test/java/ee/ria/eidas/HeartbeatIntegrationTest.java
@@ -3,6 +3,8 @@ package ee.ria.eidas;
 
 import ee.ria.eidas.config.IntegrationTest;
 import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ import java.util.Date;
 
 import static io.restassured.RestAssured.given;
 import static java.lang.Math.toIntExact;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.text.MatchesPattern.matchesPattern;
 
@@ -19,46 +22,24 @@ import static org.hamcrest.text.MatchesPattern.matchesPattern;
 public class HeartbeatIntegrationTest extends TestsBase {
 
     @Test
-    public void heartbeatIsUp() {
-        given()
+    public void heartbeatElementsPresence() {
+        Response heartBeat = given()
                 .filter(new AllureRestAssured())
                 .when().get(testEidasClientProperties.getHealthcheckUrl())
-                .then().log().ifValidationFails().statusCode(200)
+                .then().log().ifValidationFails()
+                .statusCode(200)
                 .header("Content-Type", equalTo("application/json"))
-                .body("status", equalTo("UP"),
-                        "name", equalTo("eidas-client-webapp"),
-                        "version", matchesPattern("^[0-9].[0-9].[0-9](-[a-zA-Z0-9]*)?$"),
-                        "buildTime", lessThan(toIntExact(new Date().getTime() / 1000)),
-                        "startTime", lessThanOrEqualTo(toIntExact(new Date().getTime() / 1000)),
-                        //Hope clocks are in sync
-                        "currentTime", lessThanOrEqualTo(toIntExact(new Date().getTime() / 1000)),
-                        "dependencies.size()", equalTo(2),
-                        "dependencies[0].name", equalTo("eIDAS-Node"),
-                        "dependencies[0].status", equalTo("UP"),
-                        "dependencies[1].name", equalTo("hazelcast"),
-                        "dependencies[0].status", equalTo("UP")
-                );
-    }
+                .extract().response();
 
-    @Test
-    public void heartbeatAsJsonUrl() {
-        given()
-                .filter(new AllureRestAssured())
-                .when().get(testEidasClientProperties.getHealthcheckUrl())
-                .then().log().ifValidationFails().statusCode(200)
-                .header("Content-Type", equalTo("application/json"))
-                .body("status", equalTo("UP"),
-                        "name", equalTo("eidas-client-webapp"),
-                        "version", matchesPattern("^[0-9].[0-9].[0-9](-[a-zA-Z0-9]*)?$"),
-                        "buildTime", lessThan(toIntExact(new Date().getTime() / 1000)),
-                        "startTime", lessThanOrEqualTo(toIntExact(new Date().getTime() / 1000)),
-                        "currentTime", lessThanOrEqualTo(toIntExact(new Date().getTime() / 1000)),
-                        "dependencies.size()", equalTo(2),
-                        "dependencies[0].name", equalTo("eIDAS-Node"),
-                        "dependencies[0].status", equalTo("UP"),
-                        "dependencies[1].name", equalTo("hazelcast"),
-                        "dependencies[0].status", equalTo("UP")
-                );
+        assertThat(heartBeat.body().jsonPath().get("status").toString(), Matchers.equalTo("UP"));
+        assertThat(heartBeat.body().jsonPath().get("name"), Matchers.notNullValue());
+        assertThat(heartBeat.body().jsonPath().get("version"), Matchers.notNullValue());
+        assertThat(heartBeat.body().jsonPath().get("buildTime"), Matchers.notNullValue());
+        assertThat(heartBeat.body().jsonPath().get("startTime"), Matchers.notNullValue());
+        assertThat(heartBeat.body().jsonPath().get("currentTime"), Matchers.notNullValue());
+        assertThat(heartBeat.body().jsonPath().get("dependencies.name"), Matchers.hasItem("credentials"));
+        assertThat(heartBeat.body().jsonPath().get("dependencies.name"), Matchers.hasItem("hazelcast"));
+        assertThat(heartBeat.body().jsonPath().get("dependencies.name"), Matchers.hasItem("eIDAS-Node"));
     }
 
     @Test
